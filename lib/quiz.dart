fc4961b5c7cd4be98fbd0b3071cdb2d3003abeae
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2023-11-30 18:42:19 +0100

    013

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -21,8 +21,15 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+
+
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -36,9 +43,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
