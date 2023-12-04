93ff6acb138e22574d51e79814c2a0f259b2cc41
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2023-12-04 16:48:52 +0100

    026

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,9 @@ import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +17,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion(){
+  void answerQuestion(String selectedAnswer){
+    widget.onSelectAnswer('...');
     // currentQuestionIndex = currentQuestionIndex+1;
     // currentQuestionIndex+=1;
     setState((){
@@ -48,7 +51,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           ),
           const SizedBox(height: 30),
           ...currentQuestion.getShuffledAnswers().map((answer) {
-            return AnswerButton(answerText: answer, onTap: answerQuestion );
+            return AnswerButton(answerText: answer, onTap: (){
+              answerQuestion(answer);
+            } );
           })
         ],
       ),
