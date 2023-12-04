98e88fd10049412b8b43f0b26de0a7e5d5c4d401
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2023-12-04 18:40:16 +0100

    033

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -9,7 +9,15 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['question'] as int) + 1).toString()),
+            Text(((data['question_index'] as int) + 1).toString()),
+            Column(children: [
+              Text(data['question'] as String),
+              SizedBox(
+                height: 5,
+              ),
+              Text(data['user_answer'] as String),
+              Text(data['correct_answer'] as String),
+            ])
           ],
         );
       }).toList(),
